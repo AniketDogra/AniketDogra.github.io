bd2fdf42ac92bb563eb1a72a1002ff98873e05e5
AniketDogra <aniket.ddogra@gmail.com>
2020-07-25 20:49:38 +0530

    Update script.js

--- dip/script.js
+++ dip/script.js
@@ -61,6 +61,8 @@ function rot() {
 
 function imgThresh() {
     let dst = new cv.Mat();
+    let mat1 = new cv.Mat();
+    cv.cvtColor(mat, mat1, cv.COLOR_RGBA2GRAY, 0);
     cv.threshold(mat, dst, 177, 200, cv.THRESH_BINARY);
     cv.imshow('canvasOutput', dst);
     dst.delete();
